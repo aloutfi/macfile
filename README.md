46d449114ec11fa157274df06c265ee504765143
Andrew Loutfi <loofinator@gmail.com>
2022-03-19 16:34:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Macfile
+
+A simple repo to provide functionality for configuring a new mac for development.
+
+## tl;dr 
+**Warning:** if you are not the creator of this script, you should probably read through what is being installed. 
+
+1. Fill in github user, email, and org in macfile.sh
+2. Ensure zsh is already installed (default shell on MacOS 12)
+3. Check to see if anything has been installed already.
+4. run './macfile.sh'
+
+# TODO:
+I will probably not get all of these, but it's a list of config I'll need to do anyways.
+- gcloud setup
+- zshrc plugins
+- "open with vs code" macos automator
+- 24 hour clock with seconds
+- have application X always open file Y
+- set default browser
+- brave browser settings:
+    - disable built in password management
+    - 1password plugin
+    - import bookmarks
+- cat:
+    - zshrc
+- copy: 
+    - vimrc
+    - ssh config
+    
---
+++ macfile.sh
@@ -0,0 +1,201 @@
+GITHUB_EMAIL = ''
+GITHUB_USER = '' # Your GitHub Username
+GITHUB_ORG = '' # Your GitHub Organization
+
+
+echo "Creating an SSH key for you..."
+ssh-keygen -t rsa
+
+echo "Installing xcode-stuff"
+xcode-select --install
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update homebrew recipes
+echo "Updating homebrew..."
+brew update
+
+echo "Installing Git..."
+brew install git
+
+echo "Git config"
+
+git config --global user.name $GITHUB_USER
+git config --global user.email $GITHUB_EMAIL
+
+brew tap textualize/rich
+
+formulae=(
+  docker
+  htop
+  jupyterlab
+  netcat
+  nmap
+  node
+  poetry
+  pre-commit
+  pyenv
+  pyenv-virtualenv
+  rich
+  speedtest-cli
+  streamlink
+  thefuck
+  tldr
+  tree
+  wget
+  youtube-dl
+  zsh-syntax-highlighting
+  zsh-autosuggestions
+)
+
+
+# Install casks
+### Get cask-versions for brave-browser-nightly
+brew tap homebrew/cask-versions
+
+casks=(
+  1password
+  anki
+  brave-browser-nightly
+  deluge
+  discord
+  docker
+  eul
+  google-drive
+  macvim
+  notion
+  pycharm
+  runelite
+  signal
+  slack
+  spotify
+  tableplus
+  typora
+  visual-studio-code
+  vlc
+)
+
+echo "installing apps with Cask..."
+brew install --cask ${casks[@]}
+
+echo "Cleaning up brew"
+brew cleanup
+
+
+echo "set up workspace"
+mkdir ~/WorkSpace
+cd ~/WorkSpace
+# https://stackoverflow.com/a/32803025/9637992
+curl "https://api.github.com/orgs/$GITHUB_ORG/repos?per_page=1000" | grep -o 'git@[^"]*' | xargs -L1 git clone
+
+
+echo "Setting some Mac settings..."
+
+#"Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+#"Check for software updates daily, not just once per week"
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+#"Disable smart quotes and smart dashes as they are annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+#"Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+#"Disabling press-and-hold for keys in favor of a key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+#"Setting trackpad & mouse speed to a reasonable number"
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+#"Showing icons for hard drives, servers, and removable media on the desktop"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+
+#"Showing all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+#"Disabling the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+#"Use column view in all Finder windows by default"
+defaults write com.apple.finder FXPreferredViewStyle Clmv
+
+#"Avoiding the creation of .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+#"Enabling snap-to-grid for icons on the desktop and in other icon views"
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+#"Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+defaults write com.apple.dock tilesize -int 36
+
+#"Speeding up Mission Control animations and grouping windows by application"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock "expose-group-by-app" -bool true
+
+#"Setting Dock to auto-hide and removing the auto-hiding delay"
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+#"Setting email addresses to copy as 'foo@example.com' instead of 'Foo Bar <foo@example.com>' in Mail.app"
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+#"Enabling UTF-8 ONLY in Terminal.app and setting the Pro theme by default"
+defaults write com.apple.terminal StringEncodings -array 4
+defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
+
+#"Disable the sudden motion sensor as its not useful for SSDs"
+sudo pmset -a sms 0
+
+#"Speeding up wake from sleep to 24 hours from an hour"
+# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
+sudo pmset -a standbydelay 86400
+
+#"Setting screenshots location to ~/Desktop"
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+
+#"Setting screenshot format to PNG"
+defaults write com.apple.screencapture type -string "png"
+
+#"Enabling Safari's debug menu"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+#"Removing useless icons from Safari's bookmarks bar"
+defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+
+#"Enabling the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+
+#"Use `~/Downloads/Incomplete` to store incomplete downloads"
+defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Incomplete"
+
+#"Don't prompt for confirmation before downloading"
+defaults write org.m0k.transmission DownloadAsk -bool false
+
+#"Trash original torrent files"
+defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+
+#"Hide the donate message"
+defaults write org.m0k.transmission WarningDonate -bool false
+
+#"Hide the legal disclaimer"
+defaults write org.m0k.transmission WarningLegal -bool false
+
+killall Finder
+
+echo "Done!"
